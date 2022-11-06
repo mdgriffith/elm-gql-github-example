2a6af463578d3d4d876c60df346c54d0e4a94323
Matthew Griffith <mdg.griffith@gmail.com>
2022-11-06 09:16:52 -0500

    fix quoting in bash scripts

--- scripts/api-full-refresh.sh
+++ scripts/api-full-refresh.sh
@@ -4,4 +4,4 @@
 # This takes a decent amount of time (5 seconds last time I tried)
 # And the schema doesn't change very often, so you don't need to do this in your dev loop.
 # This is what you want to run in CI though.
-elm-gql run https://api.github.com/graphql --namespace GitHub --header 'Authorization: bearer $GITHUB_ACCESS_TOKEN'
+elm-gql run https://api.github.com/graphql --namespace GitHub --header "Authorization: bearer $GITHUB_ACCESS_TOKEN"
--- scripts/api-init.sh
+++ scripts/api-init.sh
@@ -2,4 +2,4 @@
 
 # Same as Full Refresh, but creates a few files that you will need to manage once they're created.
 # This repo has already done this part, but I figured it's useful to see it!
-elm-gql init https://api.github.com/graphql --namespace GitHub --header 'Authorization: bearer $GITHUB_ACCESS_TOKEN'
+elm-gql init https://api.github.com/graphql --namespace GitHub --header "Authorization: bearer $GITHUB_ACCESS_TOKEN"
